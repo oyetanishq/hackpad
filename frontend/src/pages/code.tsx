3c24c97c8cea8769807bcd1ba5188d93f24111c5
Tanishq Singh <hi@tanishqsingh.com>
2025-02-22 02:51:38 +0530

    codeeditor sync but no saving

--- frontend/src/pages/code.tsx
+++ frontend/src/pages/code.tsx
@@ -1,12 +1,27 @@
 import { IProject } from "@/components/dashboard/project-card";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import isDesktop from "@/lib/isDesktop";
-import CodeEditorForDesktop from "@/components/editor/desktop";
-import CodeEditorForMobile from "@/components/editor/mobile";
 import Header from "@/components/header";
 import classNames from "classnames";
 import { ChatBubbleLeftEllipsisIcon, FolderIcon } from "@heroicons/react/24/outline";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs";
+import "prismjs/themes/prism-dark.css";
+
+import "prismjs/components/prism-typescript";
+
+export type Tab = {
+	id: number | string;
+	active: boolean;
+	name: string;
+	code: string;
+	language: string;
+};
+
+export type Chat = {
+	from: string;
+	message: string;
+};
 
 enum IExplorer {
 	"chat",
@@ -14,94 +29,33 @@ enum IExplorer {
 	"null",
 }
 
-const content = {
-	"index.ts": "const a = 0;",
-	folder1: {
-		"app.tsx": 'export default function () {\n    return "hi";\n}',
-		folder2: {
-			"style.css": "body {\n    margin: 0;\n}",
-			folder1: {
-				"app.tsx": 'export default function () { return "hi"; }',
-				folder2: {
-					"style.css": "body { margin: 0; }",
-				},
-			},
-		},
-	},
-};
-
-const FileNode = ({ name, content, onFileClick, path }: any) => {
-	const [isOpen, setIsOpen] = useState(false);
-
-	const isFolder = typeof content === "object";
-	const currentPath = path ? `${path}/${name}` : name;
-
-	return (
-		<div className="pl-4">
-			<div className={`flex items-center cursor-pointer text-base ${isFolder ? "font-semibold" : ""}`} onClick={() => (isFolder ? setIsOpen(!isOpen) : onFileClick(currentPath))}>
-				{isFolder ? <span className="mr-2">{isOpen ? "📂" : "📁"}</span> : <span className="mr-2">📄</span>}
-				{name}
-			</div>
-			{isFolder && isOpen && (
-				<div className="ml-4">
-					{Object.entries(content).map(([childName, childContent]) => (
-						<FileNode key={childName} name={childName} content={childContent} onFileClick={onFileClick} path={currentPath} />
-					))}
-				</div>
-			)}
-		</div>
-	);
-};
-
-const FileExplorer = ({ structure, className, onFileClick }: any) => {
-	return (
-		<div className={className}>
-			<h2 className="text-base font-bold mb-4">File Explorer</h2>
-			{Object.entries(structure).map(([name, content]) => (
-				<FileNode key={name} name={name} content={content} onFileClick={onFileClick} path="" />
-			))}
-		</div>
-	);
-};
-
-const getFileContentByPath = (structure: any, path: string): string => {
-	const parts = path.split("/");
-	let current = structure;
-	for (const part of parts) {
-		if (typeof current === "object" && part in current) {
-			current = current[part];
-		} else {
-			return ""; // File not found
-		}
-	}
-	return typeof current === "string" ? current : ""; // Return content if it's a file
-};
-
-const updateFileContentByPath = (structure: any, path: string, newContent: string) => {
-	const parts = path.split("/");
-	let current = structure;
-
-	for (let i = 0; i < parts.length - 1; i++) {
-		const part = parts[i];
-		if (typeof current === "object" && part in current) {
-			current = current[part];
-		} else {
-			return false; // Path invalid
-		}
-	}
-	const fileName = parts[parts.length - 1];
-	if (fileName in current) {
-		current[fileName] = newContent;
-	}
-};
-
 export default function Code() {
 	const { id } = useParams();
+	const codeId = id;
 	const [project, setProject] = useState<IProject>();
-	const [file, setFile] = useState<{ name: string; content: string }>();
 	const [explorer, setExplorer] = useState<IExplorer>(IExplorer.files);
 
+	const [socket, setSocket] = useState<WebSocket | null>(null);
+	const [tabs, setTabs] = useState<Tab[]>([]);
+
+	const [chats, setChats] = useState<Chat[]>([]);
+	const [message, setMessage] = useState("");
+
 	useEffect(() => {
+		const ws = new WebSocket("wss://simple-ws.deno.dev");
+		ws.onopen = () => console.log("connected");
+		ws.onclose = () => console.log("closed");
+		ws.onmessage = ({ data }) => {
+			const message = JSON.parse(data).message;
+			if (message.rid !== codeId) return;
+
+			if (message.type === "code-update") setTabs((tabs) => tabs.map(({ id, name, code, language, active }) => ({ id, name, active, code: name === message.name ? message.code : code, language })));
+			if (message.type === "create-file") setTabs((tabs) => [...tabs, { active: false, name: message.name, language: "typescript", code: "", id: Math.random() * 10 }]);
+			if (message.type === "message") setChats((chats) => [...chats, { from: message.name, message: message.message }]);
+		};
+
+		setSocket(ws);
+
 		(async () => {
 			try {
 				const token = localStorage.getItem("token")!;
@@ -122,15 +76,11 @@ export default function Code() {
 				alert((error as Error).message);
 			}
 		})();
+
+		return () => ws.close();
 	}, []);
 
-	useEffect(() => {
-        if (!file) return;
-        updateFileContentByPath(content, file.name, file.content)
-        
-    }, [file]);
-
-	if (!project) return "loading...";
+	if (!project || !socket) return "loading...";
 
 	return (
 		<div className="h-full w-full rounded-md bg-white flex flex-col">
@@ -141,30 +91,110 @@ export default function Code() {
 					<ChatBubbleLeftEllipsisIcon onClick={() => setExplorer((exp) => (exp === IExplorer.chat ? IExplorer.null : IExplorer.chat))} className={classNames("size-5 cursor-pointer")} />
 				</div>
 
-				<FileExplorer
-					onFileClick={(filePath: string) => {
-						console.log(filePath);
-						setFile({ name: filePath, content: getFileContentByPath(content, filePath) });
-					}}
-					structure={content}
-					className={classNames("h-full duration-300 bg-white p-2 flex-col justify-start items-start w-56 overflow-x-scroll", explorer === IExplorer.files ? "flex" : "hidden")}
-				/>
+				<div className={classNames("h-full duration-300 bg-white p-2 flex-col justify-start items-start w-56", explorer === IExplorer.files ? "flex" : "hidden")}>
+					{tabs.map(({ id, name, active }) => {
+						return (
+							<button
+								id={"file-name-" + id}
+								type="button"
+								key={id}
+								onClick={() => setTabs((tab) => tab.map(({ id: ID, name, code, language }) => ({ id: ID, name, active: ID == id, code, language })))}
+								onDoubleClick={() => console.log("change name request")}
+								className={classNames("h-6 w-full relative flex justify-start px-4 items-center text-sm font-bold text-center", active ? "text-indigo-700" : "text-gray-600 hover:text-gray-800")}
+							>
+								{name}
+							</button>
+						);
+					})}
+					<label
+						className="mx-4 hover:rotate-90 transition duration-300 cursor-pointer h-6 w-6 rounded-md bg-rose-300 hover:bg-rose-400 text-red-600 hover:text-red-700 flex justify-center items-center font-semibold"
+						onClick={() => {
+							const name = prompt("Enter a file name: ") || "newfile";
+							socket.send(
+								JSON.stringify({
+									type: "sendtoall",
+									message: {
+										type: "create-file",
+										rid: id,
+										name,
+									},
+								})
+							);
+						}}
+					>
+						+
+					</label>
+				</div>
 
 				<div className={classNames("h-full duration-300 bg-white p-2 flex-col justify-start items-center w-56", explorer === IExplorer.chat ? "flex" : "hidden")}>
 					<p className="underline underline-offset-2">chatting</p>
 					<div className="flex-1 flex flex-col justify-end items-center w-full">
-						<div className="w-full text-sm my-2">
-							<p className="text-yellow-600 underline underline-offset-2">kaju:</p>
-							<p className="ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore facere quos, itaque dolorum inventore iste quasi obcaecati, necessitatibus corrupti libero cum impedit quod fugit adipisci ipsa reiciendis! Aperiam, sunt neque.</p>
-						</div>
+						{chats.map(({ from, message }, index) => (
+							<div key={from + message + index} className="w-full text-sm my-2">
+								<p className="text-yellow-600 underline underline-offset-2">{from}:</p>
+								<p className="ml-6">{message}</p>
+							</div>
+						))}
 					</div>
 					<div className="w-full flex gap-1 mt-3">
-						<input type="text" placeholder="Message" className="flex-1 p-1 text-sm border focus:outline-none" />
-						<button className="p-1 px-2 border">send</button>
+						<input type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" className="flex-1 p-1 text-sm border focus:outline-none" />
+						<button
+							className="p-1 px-2 border"
+							onClick={() => {
+								if (message.length < 1) return;
+
+								socket.send(
+									JSON.stringify({
+										type: "sendtoall",
+										message: {
+											type: "message",
+											rid: codeId,
+											message,
+											name: localStorage.getItem("userEmail")?.split("@")[0],
+										},
+									})
+								);
+								setMessage("");
+							}}
+						>
+							send
+						</button>
 					</div>
 				</div>
 
-				<div className="flex-1 border-l overflow-hidden">{file && <CodeEditorForDesktop value={file.content} onChange={(value: string) => setFile((f) => (f ? { name: f.name, content: value } : undefined))} />}</div>
+				<div className={classNames("flex-1 overflow-hidden", explorer === IExplorer.null ? "" : "border-l ")}>
+					{tabs.map(({ id, code, active, language, name }) => {
+						return (
+							<div key={id} className={classNames(active ? "" : "hidden", "overflow-scroll h-full w-full hide-scrollbar")}>
+								<Editor
+									className="editor"
+									value={code}
+									onValueChange={(newCode) => {
+										socket.send(
+											JSON.stringify({
+												type: "sendtoall",
+												message: {
+													type: "code-update",
+													rid: codeId,
+													name,
+													code: newCode,
+												},
+											})
+										);
+									}}
+									highlight={(code) =>
+										highlight(code, languages[language], language)
+											.split("\n")
+											.map((line, i) => `<span class='editorLineNumber'>${i + 1}</span>${line}`)
+											.join("\n")
+									}
+									padding={16}
+									tabSize={4}
+								/>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
