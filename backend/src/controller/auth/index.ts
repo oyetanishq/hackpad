e37592bad4eaf25a4558383826c9e81b674e64d7
Tanishq Singh <hi@tanishqsingh.com>
2025-06-15 00:55:02 +0530

    health-route add

--- backend/src/controller/auth/index.ts
+++ backend/src/controller/auth/index.ts
@@ -3,27 +3,32 @@ import User from "../../model/User";
 import { generateToken } from "../../lib/tokens";
 import { genSaltSync, hashSync, compareSync } from "bcrypt";
 
+/* HEALTH CHECK CONTROLLER */
+export const healthCheck: RequestHandler = async (req, res) => {
+    res.status(200).send("/auth/");
+};
+
 /**
  * @route /auth/register
  * @method POST
  * @description register a new user, and return jwt token
  */
 export const register: RequestHandler = async (req, res) => {
-	try {
-		const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-		if (await User.findOne({ email })) throw new Error("user already exists");
+        if (await User.findOne({ email })) throw new Error("user already exists");
 
-		const salt = genSaltSync(10);
-		const hashedPassword = hashSync(password, salt);
+        const salt = genSaltSync(10);
+        const hashedPassword = hashSync(password, salt);
 
-		const user = new User({ email, password: hashedPassword });
-		await user.save();
+        const user = new User({ email, password: hashedPassword });
+        await user.save();
 
-		res.status(201).json({ success: true, user: { id: user._id, email: user.email, token: generateToken(user._id as string, user.email) } });
-	} catch (error) {
-		res.status(400).json({ success: false, error: (error as Error).message });
-	}
+        res.status(201).json({ success: true, user: { id: user._id, email: user.email, token: generateToken(user._id as string, user.email) } });
+    } catch (error) {
+        res.status(400).json({ success: false, error: (error as Error).message });
+    }
 };
 
 /**
@@ -32,15 +37,15 @@ export const register: RequestHandler = async (req, res) => {
  * @description validate a user, and return jwt token
  */
 export const login: RequestHandler = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({ email });
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
 
-		if (!user) throw new Error("invalid credentials");
-		if (!compareSync(password, user.password)) throw new Error("invalid credentials");
+        if (!user) throw new Error("invalid credentials");
+        if (!compareSync(password, user.password)) throw new Error("invalid credentials");
 
-		res.status(201).json({ success: true, user: { id: user._id, email: user.email, token: generateToken(user._id as string, user.email) } });
-	} catch (error) {
-		res.status(401).json({ success: false, error: (error as Error).message });
-	}
+        res.status(201).json({ success: true, user: { id: user._id, email: user.email, token: generateToken(user._id as string, user.email) } });
+    } catch (error) {
+        res.status(401).json({ success: false, error: (error as Error).message });
+    }
 };
--- backend/src/router/auth/index.ts
+++ backend/src/router/auth/index.ts
@@ -4,6 +4,13 @@ import * as controller from "../../controller/auth/index";
 const router = Router();
 
 /**
+ * @route /auth/
+ * @method GET
+ * @description just for checking auth route
+ */
+router.get("/", controller.healthCheck);
+
+/**
  * @route /auth/register
  * @method POST
  * @description register a new user, and return jwt token
